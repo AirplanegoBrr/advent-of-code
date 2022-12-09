dc5f1b40f9193151bb89a87826da4168025c2b2e
AirplaneGoBrr <71111395+AirplanegoBrr@users.noreply.github.com>
2022-12-09 08:44:46 -0800

    ??

--- 2022/day7/index.js
+++ 2022/day7/index.js
@@ -10,21 +10,24 @@ var val = 0
 async function getSizeOfDir(dir){
     var size = 0
 
-    db.data = {}
-    await db.save()
-
     for (var a in dir){
         const type = typeof(dir[a])
         if (type == "string") {
             var s = Number(dir[a])
             size += s;
         } else if (type == "object"){
-            await getSizeOfDir(dir[a])
+            console.log(`- Running ${a}`)
+            var numBack = await getSizeOfDir(dir[a])
+            size += numBack
+            console.log("NumBack",numBack,a)
         }
     }
     if (size && size < 100000) {
         // console.log(size)
-        val += Number(size);
+        console.log("Out", size)
+        return Number(size);
+    } else {
+        return 0
     }
 }
 // 1704740
@@ -37,6 +40,9 @@ async function main(){
     const fileRaw = fs.readFileSync("input.txt").toString().split("\r\n")
     console.log(fileRaw)
 
+    db.data = {}
+    await db.save()
+
     var filePath = []
     var files = {}
 
@@ -44,9 +50,9 @@ async function main(){
         // console.log(thing)
         if (thing.startsWith("$")){
             var command = thing.replace("$ ", "")
-            // console.log(command)
+            console.log("command",command)
             if (command == "ls") {
-                
+                continue
             } else if (command.startsWith("cd")){
                 var dir = command.replace("cd ", "")
                 // console.log("DIR",dir)
@@ -57,11 +63,13 @@ async function main(){
                 } else {
                     filePath.push(dir)
                 }
+                continue
             }
         } else {
             if (thing.startsWith("dir ")){
                 var filePathDots = filePath.join(".")
                 await db.set(filePathDots, {})
+                continue
             } else {
                 // files[filePath][thing.split(" ")[0]] = thing.split(" ")[1]
                 var filePathDots = filePath.join(".")
@@ -69,15 +77,16 @@ async function main(){
                 var file = thing.replace(".", "_").split(" ")
 
                 await db.set(filePathDots+`.${file[1]}`, file[0])
+                continue
             }
         }
     }
     var sizes = {}
     var a = []
-    await db.load()
     var data = db.data
+    await db.save()
     // console.log(data)
-    await getSizeOfDir(data)
-    console.log(val)
+    var back = await getSizeOfDir(data)
+    console.log(back)
 }
 main()
